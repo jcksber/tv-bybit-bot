7bbfc1335088f7c6f3da59659c69547f95b5b20c
Jack Kasbeer <kasbeer@cmu.edu>
2021-03-11 15:13:30 -0600

    host should be localhost

--- webhook-bot.py
+++ webhook-bot.py
@@ -60,5 +60,5 @@ def webhook():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='vast-chamber-70273.herokuapp.com', port=port)
+    app.run(host='0.0.0.0', port=port)
 
